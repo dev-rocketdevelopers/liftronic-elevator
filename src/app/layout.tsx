51a40648e7c189f7f39bf9924bb03f9edb65fb93
dev.hrushikedar@gmail.com <dev.hrushikedar@gmail.com>
2026-08-19 18:18:31 +0530

    feat: add Facebook Pixel tracking script to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,6 +91,33 @@ export default function RootLayout({
           href="https://cdn.sanity.io"
           crossOrigin="anonymous"
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1581087773614821');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1581087773614821&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body
         className={`${dmSans.variable} ${playfairDisplay.variable} antialiased bg-soft text-charcoal`}
